17d5e50a56957f91b6c923af3de5bdaa13c6c4c2
Bernardo Lopes <bernardolopes@id.uff.br>
2017-01-12 11:00:38 -0200

    Primeiro teste, agora de verdade.
    (Matando o TDD arquivo prematuro por arquivo prematuro)

---
+++ src/main/java/calculadora/operacao/OperacaoFactory.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.operacao;
+
+/**
+ *
+ * @author bernardolopes at id.uff.br
+ */
+public class OperacaoFactory {
+    
+    public OperacaoFactory() {}
+    
+    public Operacao getOperacao(TipoOperacao o){
+//        switch(o){
+//            case SOMA:
+//     
+//        }
+        return null;
+    }
+    
+}
